ebf09e233516a783b6eb533b2b5d442f700745bb
valentinstoecker <valentin@stoeckermail.de>
2024-03-07 17:12:44 +0100

    add trace eq

--- test/src/io/equiv/panproc/hpfl/HPFLTest.scala
+++ test/src/io/equiv/panproc/hpfl/HPFLTest.scala
@@ -31,3 +31,31 @@ class HPFLSuite extends munit.FunSuite:
     val result = typecheck(formula)
     assert(result.isEmpty)
   }
+
+  test("typecheck trace equivalence") {
+    def or(a: HPFLCore[Char, Char, Unit], b: HPFLCore[Char, Char, Unit]) =
+      neg(and(Set(neg(a), neg(b))))
+    def biimpl(a: HPFLCore[Char, Char, Unit], b: HPFLCore[Char, Char, Unit]) =
+      and(Set(or(a, b), or(neg(a), neg(b))))
+    val formula = app(
+      app(
+        neg(mu(
+          'F',
+          neg(lam(
+            'X',
+            lam(
+              'Y',
+              and(Set(
+                biimpl(variable('X'), variable('Y')),
+                app(app(neg(variable('F')), variable('X')), variable('Y'))
+              ))
+            )
+          ))
+        )),
+        top
+      ),
+      top
+    )
+    val result = typecheck(formula)
+    assert(result.isDefined)
+  }
