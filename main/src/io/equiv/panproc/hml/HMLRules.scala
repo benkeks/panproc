5cebfc508760c7b555639d610daad90a05a6f3ab
benkeks <benjamin.bisping@tu-berlin.de>
2024-07-10 19:36:07 +0200

    Begin new infrastructure for inference rules

--- main/src/io/equiv/panproc/hml/HMLRules.scala
+++ main/src/io/equiv/panproc/hml/HMLRules.scala
@@ -4,38 +4,60 @@ import io.equiv.panproc.hml.HennessyMilnerLogic.*
 import io.equiv.panproc.ts.WeakTransitionSystem
 import io.equiv.panproc.meta.MetaSyntax.*
 
-class HMLRules[S, A, L](val ts: WeakTransitionSystem[S, A, L]):
+class HMLRules[S, A]:
 
-  case class HMLSatisfies(s: S, formula: Formula[A]) extends MetaJudgment
-  case class LTSStep(s0: S, a: A, s1: S) extends MetaJudgment
-  case class LTSInternalStep(s0: S, s1: S) extends MetaJudgment
+  def HMLSatisfies(p: MetaValuable[S], formula: MetaValuable[Formula[MetaValuable[A]]]) =
+    MetaJudgment("hml_satisfies", List(p, formula))
+  def LTSStep(p0: MetaValuable[S], a: MetaValuable[A], p1: MetaValuable[S]) =
+    MetaJudgment("lts_step", List(p0, a, p1))
+  def LTSInternalStep(p0: MetaValuable[S], p1: MetaValuable[S]) =
+    MetaJudgment("lts_internal_step", List(p0, p1))
 
-  def obsI(s: S, obs: Observe[A]) =
-    for s1 <- ts.post(s, obs.action).toList
-    yield MetaRule("obsI", List(), LTSStep(s, obs.action, s1))
+  val obsI = MetaRule("obsI",
+    List(
+      LTSStep(MetaVariable[S]("p"), MetaVariable[A]("a"), MetaVariable[S]("p'")),
+      HMLSatisfies(MetaVariable[S]("p'"), MetaVariable("𝜑"))
+    ),
+    HMLSatisfies(MetaVariable[S]("p"), MetaFactory2(
+      MetaVariable("a"), MetaVariable("a").asInstanceOf[MetaValuable[A]],
+      MetaVariable("𝜑"), Placeholder[MetaValuable[A]]("𝜑"), (a, 𝜑) => Observe(a, 𝜑))
+    )
+  )
 
-  def conjI(s: S, conj: And[A]) = List(MetaRule(
-    "conjI",
-    for c <- conj.subterms.toList yield HMLSatisfies(s, c),
-    HMLSatisfies(s, conj)
-  ))
-
-  def disjI(s: S, disj: Negate[A]) = disj match
-    case Negate(And(subterms)) =>
-      for c <- subterms.toList yield MetaRule(
-        "disjI",
-        List(HMLSatisfies(s, Negate(c))),
-        HMLSatisfies(s, disj)
+  def conjI[I](indices: List[I]) =
+    MetaRule("conjI",
+      indices.map(i =>
+        HMLSatisfies(MetaVariable[S]("p"), MetaVariable(s"𝜑_$i"))
+      ),
+      HMLSatisfies(MetaVariable[S]("p"), MetaFactoryN(
+        indices.map(i => MetaVariable(s"𝜑_$i")),
+        indices.map(i => Placeholder(s"𝜑_$i").asInstanceOf[Formula[MetaValuable[A]]]),
+        (𝜑s) => And(𝜑s.toSet))
       )
-    case _ => List()
+  )
 
-  def negObsI(s: S, negObs: Negate[A]) = negObs match
-    case Negate(Observe(action, andThen)) =>
-      List(MetaRule(
-        "negObsI",
-        for s1 <- ts.post(s, action).toList
-          yield HMLSatisfies(s1, Negate(andThen)),
-        HMLSatisfies(s, negObs)
-      ))
-    case _ => List()
+  // def conjI(s: S, conj: And[A]) = List(MetaRule(
+  //   "conjI",
+  //   for c <- conj.subterms.toList yield HMLSatisfies(s, c),
+  //   HMLSatisfies(s, conj)
+  // ))
+
+  // def disjI(s: S, disj: Negate[A]) = disj match
+  //   case Negate(And(subterms)) =>
+  //     for c <- subterms.toList yield MetaRule(
+  //       "disjI",
+  //       List(HMLSatisfies(s, Negate(c))),
+  //       HMLSatisfies(s, disj)
+  //     )
+  //   case _ => List()
+
+  // def negObsI(s: S, negObs: Negate[A]) = negObs match
+  //   case Negate(Observe(action, andThen)) =>
+  //     List(MetaRule(
+  //       "negObsI",
+  //       for s1 <- ts.post(s, action).toList
+  //         yield HMLSatisfies(s1, Negate(andThen)),
+  //       HMLSatisfies(s, negObs)
+  //     ))
+  //   case _ => List()
 
--- main/src/io/equiv/panproc/meta/MetaSyntax.scala
+++ main/src/io/equiv/panproc/meta/MetaSyntax.scala
@@ -5,19 +5,52 @@ import io.equiv.panproc.lambda.Syntax.*
 
 object MetaSyntax:
 
-  abstract sealed class MetaExpression
+  abstract sealed class MetaExpression {
+    def pretty: String
+    def prettyTex: String
+  }
 
-  /**
-    * This class is meant to be used to hook in own kinds of judgments (e.g. typing in a context etc.)
-    */
-  abstract class MetaJudgment extends MetaExpression
+  case class MetaJudgment(name: String, parameters: List[MetaValuable[_]]) extends MetaExpression {
+    def pretty: String = s"$name(${parameters.map(_.pretty).mkString(", ")})"
+    def prettyTex: String = s"$name(${parameters.map(_.prettyTex).mkString(", ")})"
+  }
+
+  abstract class MetaValuable[T] {
+    def pretty: String
+    def prettyTex: String
+  }
+
+  case class MetaVariable[T](name: String) extends MetaValuable[T]:
+    def pretty: String = name
+    def prettyTex: String = name
+
+
+  case class MetaFactoryN[TI, TO](args: List[MetaValuable[TI]], placeholders: List[TI], factory: List[TI] => TO)
+      extends MetaValuable[TO]:
+    def pretty: String = factory(placeholders).toString()
+    def prettyTex: String = factory(placeholders).toString()
+
+  case class MetaFactory1[TI1, TO](args1: MetaValuable[TI1], placeholder1: TI1, factory: TI1 => TO)
+      extends MetaValuable[TO]:
+    def pretty: String = factory(placeholder1).toString()
+    def prettyTex: String = factory(placeholder1).toString()
+
+  case class MetaFactory2[TI1, TI2, TO](args1: MetaValuable[TI1], placeholder1: TI1, args2: MetaValuable[TI2], placeholder2: TI2,
+    factory: (TI1, TI2) => TO)
+      extends MetaValuable[TO]:
+    def pretty: String = factory(placeholder1, placeholder2).toString()
+    def prettyTex: String = factory(placeholder1, placeholder2).toString()
+
+
+  case class MetaValue[T](value: T) extends MetaValuable[T]:
+    def pretty: String = value.toString()
+    def prettyTex: String = value.toString()
 
   case class MetaRule(
     name: String,
     premisses: List[MetaExpression],
     conclusion: MetaExpression
-  ) extends MetaExpression
-
-  abstract case class MetaVariable(name: String) extends Intermediate:
-    def pretty: String = name
-    def prettyTex: String = name
+  ) extends MetaExpression {
+    def pretty: String = premisses.map(_.pretty).mkString("\n") + "\n [" + name + "] --------------\n" + conclusion.pretty
+    def prettyTex: String = pretty
+  }
